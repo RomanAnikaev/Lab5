544a0e0fb9b92809e428dbe78d3a8c5ccf2e1334
RomanAnikaev <roman.anikaiev@nure.ua>
2023-04-30 18:16:54 +0300

    5th commit

---
+++ Lab5Anikaev/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Lab5Anikaev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(542, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 22);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Почати роботу програми!";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(330, 146);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(348, 155);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(455, 146);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(238, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Магазин";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(381, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Продукція магазина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(345, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Сформувати магазин ->";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(-4, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(824, 55);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button2.Location = new System.Drawing.Point(52, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 41);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Перший режим! Магазин відкрит для покупців.";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button3.Location = new System.Drawing.Point(440, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(308, 41);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Другий режим! Магазин знаходиться на переобліку.";
+            this.button3.UseCompatibleTextRendering = true;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(816, 312);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Univer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab5Anikaev/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+using NLog;
+using System.IO;
+using System.Xml.Serialization;
+using NLog.Fluent;
+
+namespace Lab5Anikaev
+{
+    public partial class Form1 : Form
+    {
+        public static Random r = new Random();
+        public static Control form1;
+        public static string[] names_prod = { "Стілець", "Стіл кухонний", "Обідній стіл", "Крісло", "Шафа", 
+            "Раковина", "Ванна", "Тумба", "Комп'ютерне крісло", "Підвісне крісло"};
+
+        List<Store> stores = new List<Store>();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        [Serializable]
+        public class Product
+        {
+            public string name_product; //Назва продукту
+            public int quantity_of_goods; //кількість товару
+            public int price; //вартість продукту для продажу
+            public int purchase_price; //закупівельна ціна
+
+            public Product() { }
+            public Product(string nm)
+            {
+                int temp = r.Next(1, 300);
+
+                Name_product = nm; //Назва продукту
+                Quantity_of_goods = temp; //кількість товару
+
+                temp = r.Next(400, 5000);
+                Price = temp; //товар для продажу
+
+                if (temp <= 1000) Purchase_price = temp - 300;
+                else if (temp > 1000 && temp <= 3000) Purchase_price = temp - 700;
+                else Purchase_price = temp - 1200;
+            }
+            public string Name_product
+            {
+                get => this.name_product;
+                set => this.name_product = value;
+            }
+            public int Quantity_of_goods
+            {
+                get => this.quantity_of_goods;
+                set => this.quantity_of_goods = value;
+            }
+            public int Price
+            {
+                get => this.price;
+                set => this.price = value;
+            }
+            public int Purchase_price
+            {
+                get => this.purchase_price;
+                set => this.purchase_price = value;
+            }
+        }
+
+        [Serializable]
+        public class Store
+        {
+            public string name_store; //Назва магазину
+            public int number_of_buyers; //Кількість покупців в магазині
+            public int cash_register; //Касса магазину
+            public List<Product> products = new List<Product>();
+
+            public Store()
+            {
+                Name_store = "Меблевий магазин";//назва магазину
+                Number_of_buyers = 0;//кількість покупців
+                Cash_register = 50000;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    products.Add(new Product(names_prod[i]) { });//un6 - тип кімнати
+                }
+            }
+            public string Name_store
+            {
+                get => this.name_store;
+                set => this.name_store = value;
+            }
+            public int Number_of_buyers
+            {
+                get => this.number_of_buyers;
+                set => this.number_of_buyers = value;
+            }
+            public int Cash_register
+            {
+                get => this.cash_register;
+                set => this.cash_register = value;
+            }
+        }
+
+        public void Works()
+        {
+            int temp = r.Next(1, 20);
+            stores[0].number_of_buyers = temp;//генерація кількості покупців
+            for (int i = 0; i < temp; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if (stores[0].products[j].quantity_of_goods > 0)
+                    {
+                        int temp1 = r.Next(1, stores[0].products[j].quantity_of_goods);
+                        stores[0].products[j].quantity_of_goods -= temp1;
+                        stores[0].cash_register += temp1 * stores[0].products[j].price;
+                        logger.Debug("Покупець номер: " + i);
+                        logger.Debug("Товар: " + stores[0].products[j].name_product);
+                        logger.Debug("Кількість купленого товару: " + temp1);
+                        logger.Debug("Кількість товару: " + stores[0].products[j].quantity_of_goods);
+                        logger.Debug("Витрати: " + temp1 * stores[0].products[j].price);
+                        logger.Debug("Каса магазину: " + stores[0].cash_register);
+                        
+                    }
+                    dataGridView1.Invoke(new MethodInvoker(delegate ()
+                    {
+                        BindingSource binding1 = new BindingSource();
+                        binding1.DataSource = stores;
+                        dataGridView1.DataSource = binding1;
+                    }));
+                    dataGridView2.Invoke(new MethodInvoker(delegate ()
+                    {
+                        BindingSource binding1 = new BindingSource();
+                        binding1.DataSource = stores[0].products;
+                        dataGridView2.DataSource = binding1;
+                    }));
+                    Thread.Sleep(2000);
+                }
+                
+                stores[0].number_of_buyers -= 1;
+            }
+            
+        }
+        private void stocktaking()
+        {
+            int temp;
+            for (int i = 0; i < 10; i++)
+            {
+                if(stores[0].cash_register > 200000)
+                {
+                    temp = r.Next(100, 200);
+                    stores[0].products[i].quantity_of_goods += temp;
+                    stores[0].cash_register -= temp * stores[0].products[i].purchase_price;
+                    logger.Debug("Магазин на переобліку: ");
+                    logger.Debug("Товар: " + stores[0].products[i].name_product);
+                    logger.Debug("Кількість купленого товару: " + temp); 
+                    logger.Debug("Кількість товару: " + stores[0].products[i].quantity_of_goods);
+                    logger.Debug("Вартість: " + temp * stores[0].products[i].purchase_price);
+                    logger.Debug("Каса магазину: " + stores[0].cash_register);
+                }
+                dataGridView1.Invoke(new MethodInvoker(delegate ()
+                {
+                    BindingSource binding1 = new BindingSource();
+                    binding1.DataSource = stores;
+                    dataGridView1.DataSource = binding1;
+                }));
+                dataGridView2.Invoke(new MethodInvoker(delegate ()
+                {
+                    BindingSource binding1 = new BindingSource();
+                    binding1.DataSource = stores[0].products;
+                    dataGridView2.DataSource = binding1;
+                }));
+                Thread.Sleep(1000);
+            }
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stores.Add(new Store() { });
+
+            BindingSource binding = new BindingSource();
+            binding.DataSource = stores;
+            dataGridView1.DataSource = binding;
+            BindingSource binding1 = new BindingSource();
+            binding1.DataSource = stores[0].products;
+            dataGridView2.DataSource = binding1;
+
+            
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(Works);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var thread = new Thread(stocktaking);
+            thread.IsBackground = true;
+            thread.Start();
+
+        }
+
+      
+    }
+}
